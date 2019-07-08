5728094e5a4d1d639d13964a7b079db6a0ccd1b6
Pinaworld <pinben02@gmail.com>
2019-07-08 16:22:09 +0200

    SingIn/SignUp Activity

--- app/src/main/java/com/example/demenagemoi/SignUp.java
+++ app/src/main/java/com/example/demenagemoi/SignUp.java
@@ -5,15 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class SignIn extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign_up);
     }
-    public void SignUpActivity(View view){
-        Intent intent = new Intent(this, SignUp.class);
+    public void SignInActivity(View view){
+        Intent intent = new Intent(this, SignIn.class);
         startActivity(intent);
     }
 }

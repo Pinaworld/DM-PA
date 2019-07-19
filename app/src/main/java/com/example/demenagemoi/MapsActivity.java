25c578cf23c5fedfdf7220cb9693de6a1d4c376b
Pinaworld <pinben02@gmail.com>
2019-07-19 10:37:18 +0200

    UI Work

--- app/src/main/java/com/example/demenagemoi/MapsActivity.java
+++ app/src/main/java/com/example/demenagemoi/MapsActivity.java
@@ -2,11 +2,13 @@ package com.example.demenagemoi;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -37,10 +39,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.getUiSettings();
+        mMap.setMinZoomPreference(10);
         // Add a marker in Sydney and move the camera
-        LatLng esgi = new LatLng(48.849378, 2.389630);
-        mMap.addMarker(new MarkerOptions().position(esgi).title("ESGI"));
+        LatLng esgi = new LatLng(48.849378, 2.389630); //REQUEST GET LAT LONG
+        mMap.addMarker(new MarkerOptions().position(esgi)
+                .title("ESGI : 242 Rue du Faubourg Saint-Antoine, 75012 Paris") //REQUEST GET ADRESS AND NAME
+                .snippet("Cartons : 6 - Large : 2  Moyen : 1  Petit : 3")); //REQUEST GET CARBOARD AND SIZE
         mMap.moveCamera(CameraUpdateFactory.newLatLng(esgi));
-    }
+
+        LatLng second = new LatLng(48.854399, 2.393400);
+        mMap.addMarker(new MarkerOptions().position(second)
+                .title("Conforama : 73 Avenue Philippe-Auguste, 75011 Paris")
+                .snippet("Cartons : 23 - Large : 4  Moyen : 16  Petit : 3"));
+
+        LatLng third = new LatLng(48.854406, 2.385806);
+        mMap.addMarker(new MarkerOptions().position(third)
+                .title("OFFICE DEPOT : 190 Boulevard Voltaire, 75011 Paris")
+                .snippet("Cartons : 17 - Large : 4  Moyen : 10  Petit : 3"));
+        }
+
 }

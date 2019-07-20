49a4560f53c5e8fa9ac9962e7db4c0e1f76c0543
Sébastien Delbé <sebas9241@hotmail.fr>
2019-07-20 11:07:36 +0200

    delete api

--- DM_API/routes/index.js
+++
@@ -1,9 +0,0 @@
-const RouteManager = function() { };
-
-RouteManager.attach = function(app) {
-  app.use('/user', require('./user'));
-  app.use('/removal', require('./removal'));
-  app.use('/company', require('./company'));
-};
-
-module.exports = RouteManager;
